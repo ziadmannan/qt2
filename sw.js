5365060b446e8297224a8edaf3200d46dc5cb43e
ziadmannan <ziad.mannan@gmail.com>
2026-02-12 23:26:09 +0000

    Fully offline

--- sw.js
+++ sw.js
@@ -1,27 +1,43 @@
-const CACHE_NAME = 'hifz-v2';
+const CACHE_NAME = 'quran-tracker-v1';
 const ASSETS = [
     './',
     './index.html',
     './surahs.json',
     './manifest.json',
+    './tick.svg',
+    './book.svg',
+    './restart.svg',
+    './filter.svg',
+    './manage.svg',
     './icon.png'
 ];
 
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+// Install event: Cache all files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Caching app shell...');
+            return cache.addAll(ASSETS);
+        })
     );
 });
 
-self.addEventListener('activate', (e) => {
-    e.waitUntil(clients.claim());
+// Activate event: Clean up old caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
+// Fetch event: Serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
